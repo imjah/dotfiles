2ae0462894fe8db50a3c324cf41b36af6431d17d
imjah <imjah@tuta.io>
2025-02-10 19:47:38 +0100

    Remove fff favorites

--- .config/profile/fff.sh
+++ .config/profile/fff.sh
@@ -1,6 +1,3 @@
 export FFF_FAV1=$(xdg-user-dir DOWNLOAD)
 export FFF_FAV2=$(xdg-user-dir PROJECTS)
-export FFF_FAV3="$XDG_CONFIG_HOME/ttv/config.yml"
-export FFF_FAV4="$XDG_CONFIG_HOME/sway/config"
-export FFF_FAV5="$XDG_CONFIG_HOME/i3status/config"
 export FFF_CD_ON_EXIT=1
